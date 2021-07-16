7860be4fa1414bafb10477907c80b82c777d345f
Carlos Ruesta <carlos@dcide.com.br>
2021-07-16 14:01:58 -0300

    tipos numericos

---
+++ tipo_number.js
@@ -0,0 +1,29 @@
+const primeiroNumero = 1;
+
+const segundoNumero = 2;
+
+const soma = primeiroNumero + segundoNumero;
+
+const ehValido = true;
+
+const objetoCarro = {
+	cor: "azul",
+	tamanho: 'grande',
+	numeroDeRodas: 4,
+	batido: false
+};
+
+const zero = "0";
+
+const texto = "Maria";
+
+const naoEhUmNumero = segundoNumero / zero;
+
+console.log(naoEhUmNumero);const frase = 'O nome dela é ' + texto;
+
+console.log(frase);
+
+const elemento = '<p>Aqui estamos todos os dias</p>';
+
+const frase2 = 'ç';
+

dfc2b55f7c28d533a9f6756f4451dc41d0425793
Carlos Ruesta <carlos@dcide.com.br>
2021-07-16 14:02:17 -0300

    tipos string

---
+++ strings.js
@@ -0,0 +1,28 @@
+const texto = "Maria";
+
+const frase = 'O nome dela é ' + texto;
+
+console.log(frase);
+
+const elemento = '<p>Aqui estamos todos os dias</p>';
+
+const frase2 = 'ç';
+
+// const cidade = "123";
+// const input = 123;
+//
+// console.log(cidade === input);
+
+// console.log(cidade !== input);
+
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const tamanhoDoTexto = input.length;
+
+console.log(tamanhoDoTexto);

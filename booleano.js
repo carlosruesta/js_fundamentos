65813942199f283f549e1f2ff61db82d4eec45bd
Carlos Ruesta <carlos@dcide.com.br>
2021-07-16 14:34:14 -0300

    explicacao dos tipos booleanos

---
+++ booleano.js
@@ -0,0 +1,26 @@
+let valorAluguel = 1200;
+
+let valorSupermercado = 800;
+
+const aluguelEhMeuMaiorGasto = true;
+
+const aluguelEhMaiorQueSupermercado = valorAluguel > valorSupermercado;
+
+console.log(aluguelEhMaiorQueSupermercado);
+
+if (valorAluguel > valorSupermercado) {
+	console.log('Eu moro muito bem');
+} else {
+	console.log('Eu como muito bem');
+}
+
+let numeroPaginas = 120;
+if (numeroPaginas) {
+	console.log('Eu li muitas paginas');
+} else {
+	console.log('Eu dormi muito');
+}
+
+// true -- 1 -- "qqr coisa"
+
+// false  -- 0 - ''  --  ""
